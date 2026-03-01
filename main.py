9f30feaeb0f0f510f4d285b3fc79a92be60997dd
Jorge Romero <jorgeluis2127@gmail.com>
2026-03-01 11:37:15 -0400

    Ejercicio 2 de variables de entorno

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+
+## Librerías.
+    #@ Para cargar variables de entorno.
 import os
 from dotenv import load_dotenv
+
+    #@ Carga del entorno virtual.
 load_dotenv()
 
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_USER = os.getenv('DB_USER')
+## Configuración de hardware.
+    #@ Velocidad máxima en RPM.
+MAX_SPEED_RPM = os.getenv('MAX_SPEED_RPM')
+    #@ Rango máximo de giro.
+MAX_TURN_RANGE = os.getenv('MAX_TURN_RANGE')
 
-print(DB_HOST)
-print(DB_PORT)
-print(DB_USER)
+## Muestra de configuración
+print(MAX_SPEED_RPM)
+print(MAX_TURN_RANGE)
 
